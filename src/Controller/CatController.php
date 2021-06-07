bb4a7a39562e3fc0b42f213b80bc296a9c8c4127
LucyMerlier <lucymerlier@gmail.com>
2021-06-07 16:08:04 +0200

    updated cat route

--- src/Controller/CatController.php
+++ src/Controller/CatController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route(name="cat_")
+ */
 class CatController extends AbstractController
 {
     /**
-     * @Route("/", name="cats")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
