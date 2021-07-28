9526fed1110c146ccce870f6aa7daec13828fc7a
LucyMerlier <lucymerlier@gmail.com>
2021-07-28 09:40:49 +0200

    update base material

--- src/Controller/CatController.php
+++ src/Controller/CatController.php
@@ -4,15 +4,8 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\CatRepository;
-use App\Form\CatType;
-use App\Form\HumanType;
-use App\Entity\Human;
-use App\Entity\Cat;
-use App\Repository\HumanRepository;
 
 /**
  * @Route(name="cat_")
@@ -22,14 +15,13 @@ class CatController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(/*CatRepository $catRepository, HumanRepository $humanRepository*/): Response
+    public function index(/*CatRepository $catRepository*/): Response
     {
+        $cats = [];
         // $cats = $catRepository->findAll();
-        // $humans = $humanRepository->findAll();
 
         return $this->render('cat/index.html.twig', [
-            'cats' => $cats ?? [],
-            'humans' => $humans ?? [],
+            'cats' => $cats,
         ]);
     }
 }
